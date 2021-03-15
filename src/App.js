3a91f36ac7e62c31839dca1c90578e8274ad1585
Lisaru Robert <lisaru.robert@hotmail.com>
2021-03-15 16:15:48 +0200

    Fetch coffee shop list

--- src/App.js
+++ src/App.js
@@ -1,7 +1,38 @@
 import usePosition from './components/usePosition'
+import CoffeeShops from './components/CoffeeShops'
+import { useState, useEffect } from 'react'
 
 function App() {
   const { latitude, longitude, error } = usePosition()
+  const [appState, setAppState] = useState({
+    isLoading: false,
+    coffeeShops: null,
+    coffeeShopsFetchError: null
+  })
+
+  useEffect(() => {
+    setAppState({ isLoading: true })
+    fetch('https://blue-bottle-api-test.herokuapp.com/v1/tokens', { method: 'POST' })
+      .then((response) => response.json())
+      .then((jsonData) => {
+        fetch('https://blue-bottle-api-test.herokuapp.com/v1/coffee_shops?token=' + jsonData.token)
+          .then((response) => {
+            switch (response.status) {
+              case 200:
+                response.json()
+                  .then((jsonData) => setAppState({ isLoading: false, coffeeShops: jsonData }))
+                break
+              default:
+                setAppState({
+                  isLoading: false,
+                  coffeeShopsFetchError: 'Could not fetch shop locations ' +
+                    response.status + ': ' + response.statusText
+                })
+            }
+          })
+      })
+  }, [setAppState])
+
   return (
     <div>
       <code>
@@ -9,6 +40,10 @@ function App() {
         longitude: {longitude}<br />
         error: {error}
       </code>
+
+      <CoffeeShops isLoading={appState.isLoading}
+        shops={appState.coffeeShops}
+        error={appState.coffeeShopsFetchError} />
 
     </div>
   )
---
+++ src/components/CoffeeShops.js
@@ -0,0 +1,21 @@
+const CoffeeShops = ({ isLoading, shops, error }) => {
+    if (isLoading) return <p>Fetching coffee shop list</p>
+    if (error) return <p>{error}</p>
+    if (!shops || shops.length === 0) return <p>No shops</p>
+    return (
+        <ul>
+            <h2>Coffee Shops</h2>
+            {shops.map((shop) => {
+                return (
+                    <li key={shop.id}>
+                        {shop.name}<br />
+                        {shop.x}<br />
+                        {shop.y}<br />
+                    </li>
+                )
+            })}
+        </ul>
+    )
+}
+
+export default CoffeeShops
