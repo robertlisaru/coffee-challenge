d0260b3b87d559cea4062820b78e97f7593134bf
Lisaru Robert <lisaru.robert@hotmail.com>
2021-03-18 01:10:22 +0200

    Convert MyMap into class component

--- src/components/MyMap.js
+++ src/components/MyMap.js
@@ -1,84 +1,110 @@
 import { Map, Marker, Overlay } from 'pigeon-maps'
 import Tooltip from './Tooltip'
-import { useState } from 'react'
+import React, { useState } from 'react'
 const haversine = require('haversine')
 
-const MyMap = ({ shops, userLatitude, userLongitude }) => {
-    const [shopTooltipsVisibility, setShopTooltipsVisibility] = useState([])
-    const [userTooltipVisibility, setUserTooltipVisibility] = useState(false)
-
-    const onShopMarkerClick = ({ payload }) => {
-        const index = payload
-        let arrayCopy = [...shopTooltipsVisibility]
-        arrayCopy[index] = !arrayCopy[index]
-        setShopTooltipsVisibility(arrayCopy)
+class MyMap extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            shopTooltipsVisibility: [],
+            userTooltipVisibility: false
+        }
+        this.toggleTooltipVisibility = this.toggleTooltipVisibility.bind(this);
     }
 
-    const createShopMarkers = (shops) =>
-        shops ? shops.map((shop) =>
+    render() {
+        const { shops, userLatitude, userLongitude } = this.props
+        return (
+            <Map
+                provider={this.osmProvider}
+                defaultCenter={[45.795, 24.147]}
+                defaultZoom={4}
+                width={600}
+                height={400}>
+
+                {this.createShopMarkers(shops)}
+                {this.createShopTooltips(shops)}
+                {this.createUserMarker(userLatitude, userLongitude)}
+                {this.createUserTooltip(userLatitude, userLongitude)}
+
+            </Map >
+        )
+    }
+
+    toggleTooltipVisibility({ payload }) {
+        const index = payload
+        let newArray = [...this.state.shopTooltipsVisibility]
+        newArray[index] = !newArray[index]
+        this.setState({ shopTooltipsVisibility: newArray })
+    }
+
+    createShopMarkers(shops) {
+        return shops ? shops.map((shop) =>
             <Marker
                 key={shop.id}
                 anchor={[parseFloat(shop.x), parseFloat(shop.y)]}
                 color='black'
                 payload={shop.id}
-                onClick={onShopMarkerClick}
+                onClick={this.toggleTooltipVisibility}
             />
         ) : null
+    }
 
-    const createShopTooltips = (shops) =>
-        shops ? shops.map((shop) =>
+    createShopTooltips(shops) {
+        return shops ? shops.map((shop) =>
             <Overlay key={shop.id} anchor={[parseFloat(shop.x), parseFloat(shop.y)]}>
                 <Tooltip
-                    isVisible={shopTooltipsVisibility[shop.id]}
+                    isVisible={this.state.shopTooltipsVisibility[shop.id]}
                     name={shop.name}
-                    distance={
-                        Math.round(
-                            haversine({ latitude: userLatitude, longitude: userLongitude },
-                                { latitude: parseFloat(shop.x), longitude: parseFloat(shop.y) }
-                            ) * 10) / 10 + ' km'
-                    } />
+                    distance={this.computeDistanceFromUserTo(
+                        { latitude: shop.x, longitude: shop.y }) + ' km'} />
             </Overlay>
         ) : null
+    }
 
-    const createUserMarker = (userLatitude, userLongitude) =>
-        (userLatitude && userLongitude) ?
+    createUserMarker(userLatitude, userLongitude) {
+        return (userLatitude && userLongitude) ?
             <Marker
                 anchor={[userLatitude, userLongitude]}
                 color='red'
                 payload={'user'}
                 onClick={() => {
-                    setUserTooltipVisibility(!userTooltipVisibility)
+                    this.setState(state => ({ userTooltipVisibility: !state.userTooltipVisibility }))
                 }}
             />
             : null
+    }
 
-    const createUserTooltip = (userLatitude, userLongitude) =>
-        (userLatitude && userLongitude) ?
+    createUserTooltip(userLatitude, userLongitude) {
+        return (userLatitude && userLongitude) ?
             <Overlay anchor={[userLatitude, userLongitude]}>
                 <Tooltip
-                    isVisible={userTooltipVisibility}
+                    isVisible={this.state.userTooltipVisibility}
                     name={'Your location'}
                     distance={null} />
             </Overlay>
             : null
+    }
 
-    return <Map
-        provider={osmProvider}
-        defaultCenter={[45.795, 24.147]}
-        defaultZoom={4}
-        width={600}
-        height={400}>
+    computeDistanceFromUserTo({ latitude, longitude }) {
+        return Math.round(
+            haversine(
+                {
+                    latitude: this.props.userLatitude,
+                    longitude: this.props.userLongitude
+                },
+                {
+                    latitude: latitude,
+                    longitude: longitude
+                }
+            ) * 10) / 10
+    }
 
-        {createShopMarkers(shops)}
-        {createShopTooltips(shops)}
-        {createUserMarker(userLatitude, userLongitude)}
-        {createUserTooltip(userLatitude, userLongitude)}
-    </Map >
-}
-
-const osmProvider = (x, y, z) => {
-    const s = String.fromCharCode(97 + ((x + y + z) % 3))
-    return `https://${s}.tile.openstreetmap.org/${z}/${x}/${y}.png`
+    osmProvider(x, y, z) {
+        const s = String.fromCharCode(97 + ((x + y + z) % 3))
+        return `https://${s}.tile.openstreetmap.org/${z}/${x}/${y}.png`
+    }
 }
 
 export default MyMap
