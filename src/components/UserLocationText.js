8117c6c1de11365a0e61bf9ff865c44a39a27887
Lisaru Robert <lisaru.robert@hotmail.com>
2021-03-17 20:40:21 +0200

    Move location error/loading into separate function

--- src/components/UserLocationText.js
+++ src/components/UserLocationText.js
@@ -1,12 +1,20 @@
 const UserLocationText = ({ userLocationState }) => {
     const { latitude, longitude, isLoading, error } = userLocationState
-    if (isLoading) return <div>Locating...</div>
-    if (error) return <div>Error: {error}</div>
+    if (isLoading) return LocationLoadingMessage()
+    if (error) return LocationErrorMessage(error)
     return (
         <div>
             Your location: {latitude}, {longitude}
         </div>
     )
+}
+
+const LocationLoadingMessage = () => {
+    return <div>Locating...</div>
+}
+
+const LocationErrorMessage = (error) => {
+    return <div>Error: {error}</div>
 }
 
 export default UserLocationText
